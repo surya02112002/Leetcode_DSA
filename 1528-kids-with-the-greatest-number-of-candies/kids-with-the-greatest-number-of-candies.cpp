8a45db3cf490a6f1d55fa9b567e390e9815aaad7
Surya kant Pandey <120626821+surya02112002@users.noreply.github.com>
2024-02-01 02:39:40 +0530

    Time: 6 ms (17.32%) | Memory: 11 MB (5.58%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int max=-1;
+        for(int i=0; i< candies.size(); i++){
+            if(candies[i]>max){
+                max=candies[i];
+            }
+    
+        }
+        vector <bool> ans(candies.size());
+        for(int i=0;i<candies.size();i++){
+            int sum= candies[i]+extraCandies;
+            if(sum>=max){
+                ans[i]=true;
+            }
+            else{
+                ans[i]= false;
+            }
+        }
+        return ans;
+    }
+};
