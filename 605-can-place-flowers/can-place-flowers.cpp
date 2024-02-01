ef3b2c60a2f55eb441879f52b29fd3599ff14271
Surya kant Pandey <120626821+surya02112002@users.noreply.github.com>
2024-02-01 23:38:37 +0530

    Time: 11 ms (78.70%) | Memory: 22.5 MB (18.54%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -0,0 +1,58 @@
+// class Solution {
+// public:
+//     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+//         if(n==0){
+//             return true;
+//         }
+//         for(int i=0;i<flowerbed.size();i++){
+//             if(flowerbed[i]==0){
+//                 bool left= (i==0)||(flowerbed[i-1]==0);
+//                 bool right =(i==flowerbed.size()-1)|| (flowerbed[i+1]==0);
+//             }
+//             if(left && right){
+//                 flowerbed[i]=1;
+//                 n--;
+//                 if(n==0){
+//                     return true;
+
+//                 }
+//             }
+//         }
+//         return false;
+        
+//     }
+// };
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        
+        int l = flowerbed.size();
+        
+        if(n == 0)
+            return true;
+        
+        
+        for(int i = 0; i<l; i++) {
+            
+            if(flowerbed[i] == 0) {
+                
+                bool leftEmpty   = (i == 0) || (flowerbed[i-1] == 0);
+                
+                bool rightEmpty  = (i == l-1) || (flowerbed[i+1] == 0);
+                
+                if(leftEmpty && rightEmpty) {
+                    flowerbed[i] = 1;
+                    n--;
+                    if(n == 0)
+                        return true;
+                }
+                
+            }
+            
+        }
+        
+        return false;
+        
+    }
+};
