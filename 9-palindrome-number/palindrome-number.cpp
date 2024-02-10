272984f1cf6aa297b9596b4344ee7d12e2f95fe9
Surya kant Pandey <120626821+surya02112002@users.noreply.github.com>
2024-02-10 16:49:36 +0530

    Time: 44 ms (5.57%) | Memory: 9.2 MB (8.02%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool check(int x, int len){
+        if(x<0) return false;
+        if (len == 0 || len == 1 ) return true;
+        
+        int last = x%10;
+        int front = x/pow(10,(len-1));
+        
+        cout<<" "<<front <<" "<<last;
+        if(last != front ) return false;
+        else {
+            x = x - front*(pow(10,len-1)); x=x/10; len = len-2;
+            return check(x,len);
+        };
+    }
+
+    bool isPalindrome(int x) {
+        int len = to_string(x).length();
+        return check(x, len);
+    }
+};
