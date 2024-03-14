232fb3ba46a7abb0d404fdcbbe28c9850f38b587
Surya kant Pandey <120626821+surya02112002@users.noreply.github.com>
2024-03-15 00:52:20 +0530

    Time: 12 ms (38.84%) | Memory: 21.5 MB (71.49%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -12,23 +12,24 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        // If the tree is empty, we can say it’s balanced...
-        if (root == NULL)  return true;
-        // Height Function will return -1, when it’s an unbalanced tree...
-		if (Height(root) == -1)  return false;
-		return true;
-	}
-    // Create a function to return the “height” of a current subtree using recursion...
-	int Height(TreeNode* root) {
-        // Base case...
-		if (root == NULL)  return 0;
-        // Height of left subtree...
-		int leftHeight = Height(root->left);
-        // Height of height subtree...
-		int rightHight = Height(root->right);
-        // In case of left subtree or right subtree unbalanced or their heights differ by more than ‘1’, return -1...
-		if (leftHeight == -1 || rightHight == -1 || abs(leftHeight - rightHight) > 1)  return -1;
-        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
-		return max(leftHeight, rightHight) + 1;
+        return dfsTree(root)!=-1;
+    }
+    int dfsTree(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        int lh= dfsTree(root->left);
+        if(lh==-1){
+            return-1;
+        }
+        int rh =dfsTree(root->right);
+        if(rh==-1){
+            return -1;
+        }
+
+        if(abs(lh-rh)>1){
+            return -1;
+        }
+        return max(lh,rh)+1;
     }
 };
