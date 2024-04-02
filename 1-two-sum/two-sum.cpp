507700af209702f9ad9c0d0ebed4eb1e839ec9db
Surya kant Pandey <120626821+surya02112002@users.noreply.github.com>
2024-04-03 00:23:30 +0530

    Time: 3 ms (98.69%) | Memory: 14 MB (29.39%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,7 +5,8 @@ public:
         unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++)
         {
-            int more = target-nums[i];
+            int more = target-nums[i];  
+            
             if(mp.find(more)!=mp.end())
             {
                 return {i,mp[more]};
