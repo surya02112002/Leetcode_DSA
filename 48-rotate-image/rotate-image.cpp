89d48e9cbbef532b29feccd6b0dda9bb4472094e
Surya kant Pandey <120626821+surya02112002@users.noreply.github.com>
2024-09-02 15:25:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (52.37%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void transpose(vector<vector<int>>& m)
+    {
+        int n=m.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j>i)
+                {
+                    swap(m[i][j],m[j][i]);
+                }
+            }
+        }
+    }
+    void rotate(vector<vector<int>>& matrix) {
+        transpose(matrix);
+        int n=matrix.size();
+        int i=0,j=n-1;
+        while(i<j)
+        {
+            for(int k=0;k<n;k++)
+            {
+                swap(matrix[k][i],matrix[k][j]);
+            }
+            i++;
+            j--;
+        }
+    }
+};
