ab7fabb78e6b50c993fca7dce0e074a3edee1211
Surya kant Pandey <120626821+surya02112002@users.noreply.github.com>
2024-11-21 22:29:58 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (86.84%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n= s.size();
+        int left=0;
+        int right= n-1;
+        for(int i=0; i<n/2;i++){
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+        
+    }
+};
