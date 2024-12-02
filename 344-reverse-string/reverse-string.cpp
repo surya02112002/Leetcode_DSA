fd7173ffe3211fa5bf432eb55e33648e2707586d
Surya kant Pandey <120626821+surya02112002@users.noreply.github.com>
2024-12-02 22:10:28 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (86.65%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -4,11 +4,14 @@ public:
         int n= s.size();
         int left=0;
         int right= n-1;
-        for(int i=0; i<n/2;i++){
+        for(int i=0;i<n/2;i++){
             swap(s[left], s[right]);
             left++;
             right--;
+            
         }
         
     }
 };
+
+
