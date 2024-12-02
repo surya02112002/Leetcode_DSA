d403b07d1b6c6f8376437b7fbf18fb64823abb6c
Surya kant Pandey <120626821+surya02112002@users.noreply.github.com>
2024-12-02 23:43:39 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (15.57%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        int i,j;
-        for(int i=0;i<arr.size();i++){
-            for(int j=0;j<arr.size();j++){
-                if(i!=j && arr[i]==2*arr[j]){
-                    return true;
-                }
-            }
-        }return false;
+    unordered_set<int> s;  
+
+    for (int num : arr) {
+        
+        if (s.count(2 * num) || (num % 2 == 0 && s.count(num / 2))) {
+            return true;
+        }
+        s.insert(num);  
     }
+    return false;
+}
 };
+
