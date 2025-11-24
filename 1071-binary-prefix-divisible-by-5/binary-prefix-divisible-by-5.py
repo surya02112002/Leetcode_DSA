ad089f0a79fdd18516b8601794993956defd5a0b
Surya kant Pandey <120626821+surya02112002@users.noreply.github.com>
2025-11-24 19:17:55 +0530

    Time: 3 ms (82.24%) | Memory: 19.1 MB (88.42%) - LeetSync

---
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+        ans = []
+        current = 0
+        
+        for bit in nums:
+            current = (current * 2 + bit) % 5   # store only remainder
+            ans.append(current == 0)
+        
+        return ans
