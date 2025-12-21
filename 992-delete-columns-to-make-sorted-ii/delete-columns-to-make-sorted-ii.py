85c561a577a512399562d497e47966f179a43398
Surya kant Pandey <120626821+surya02112002@users.noreply.github.com>
2025-12-21 11:35:34 +0530

    Time: 1 ms (82.05%) | Memory: 17.5 MB (100.00%) - LeetSync

---
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minDeletionSize(self, strs):
+        n = len(strs)
+        m = len(strs[0])
+        
+        sorted_pairs = [False] * (n - 1)
+        deletions = 0
+        
+        for col in range(m):
+            bad = False
+            for i in range(n - 1):
+                if not sorted_pairs[i] and strs[i][col] > strs[i + 1][col]:
+                    bad = True
+                    break
+            
+            if bad:
+                deletions += 1
+                continue
+            for i in range(n - 1):
+                if not sorted_pairs[i] and strs[i][col] < strs[i + 1][col]:
+                    sorted_pairs[i] = True
+        
+        return deletions
